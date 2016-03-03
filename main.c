64171fe47bc380a2fe84e83e522648eef553b003
Shinichi Wada <kirin@nts.ricoh.co.jp>
2016-03-03 15:41:33 +0900

    no message

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ printf("hello world\n");
 printf("hello world2\n");
 printf("hello world3\n");
 printf("hello world4\n");
-printf("slack test\n");
+printf("slack test\n");	
+printf("slack test2\n");
 	
 }
