b6d8ce66ae0a2387ff7dd0bcfa0df4c68c4f1555
Shin-ichi Wada <kirin@nts.ricoh.co.jp>
2017-07-07 14:30:26 +0900

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,6 @@ printf("Pull Request:`topic2`\n");
 printf("Pull Request NO.5\n");
 printf("Pull Request NO.6\n");
 	
-printf("Pull Request topic3 desuyone 16\n");
+printf("Pull Request topic3 desuyone 17\n");
 #comment13
 }
